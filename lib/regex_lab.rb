e9f70fe85123240e24f0e43c086e788a90605c66
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-24 18:48:38 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,35 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]\w+/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/[A-Z]\w+[,.?]/)
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/([0-9] *?){10}/)
+    return true
+  elsif phone.match(/[(]\d+[)]\d+-\d{4}\b/)
+    return true
+  elsif phone.match(/[(]\d+[)]\d+{7}/)
+    return true
+  else
+    return false
+  end
 end
