9bf7d75528606a4ba1838823bd83d5a352ebe154
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-24 17:22:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,10 +7,11 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.grep(/w{5}/)
+  text.grep(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  
 end
 
 def valid_phone_number?(phone)
